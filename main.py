a02143141c6c2c9467d02b2506b6a3b42ef00383
aggravatedassaultduck <144288574+personnosrep@users.noreply.github.com>
2025-06-25 19:03:56 -0700

    Update main.py

--- main.py
+++ main.py
@@ -7,20 +7,17 @@ import time
 import ctypes
 
 PROCNAME = "Spotify.exe"
-
 user32 = ctypes.WinDLL('user32')
-
-# add
-exceptions = ['DDNet CLient']
+exceptions = ['DDNet Client']
 
 def get_window_title(hwnd):
+    if not win32gui.IsWindow(hwnd):
+        return ""
+    
     length = user32.GetWindowTextLengthW(hwnd)
-
-    # incase spotify freezes or something
     if length == 0:
         return ""
 
-    #get the name
     buffer = ctypes.create_unicode_buffer(length + 1)
     user32.GetWindowTextW(hwnd, buffer, length + 1)
     return buffer.value
@@ -28,81 +25,66 @@ def get_window_title(hwnd):
 def is_spotify_ad_window(hwnd):
     if not win32gui.IsWindowVisible(hwnd):
         return False
-    
-    # Get the process ID from window handle
-    i, pid = win32process.GetWindowThreadProcessId(hwnd)
 
     try:
+        _, pid = win32process.GetWindowThreadProcessId(hwnd)
         proc = psutil.Process(pid)
         if proc.name() != PROCNAME:
             return False
-        
-    except psutil.NoSuchProcess:
+    except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
         return False
 
-    # Check for ad keywords in the window title
     title = get_window_title(hwnd)
-
-    #ads don't have a dash in their name
     return "-" not in title and title != "Spotify Free"
 
 def open_and_run():
-    # Don't wanna interrupt a match
-    for exception in exceptions:
-        if get_window_title(ctypes.windll.user32.GetForegroundWindow()) == exception:
+    # Don’t interrupt important stuff
+    foreground = ctypes.windll.user32.GetForegroundWindow()
+    fg_title = get_window_title(foreground)
+    if fg_title in exceptions:
+        return
+
+    try:
+        # Kill Spotify
+        ctypes.windll.user32.BlockInput(True)
+        for proc in psutil.process_iter(['name']):
+            if proc.info['name'] == PROCNAME:
+                proc.kill()
+
+        time.sleep(0.5)
+        os.system("start spotify")
+
+        def is_spotify_ready():
+            hwnds = []
+            win32gui.EnumWindows(lambda hwnd, _: hwnds.append(hwnd), None)
+            for hwnd in hwnds:
+                if win32gui.IsWindowVisible(hwnd):
+                    try:
+                        _, pid = win32process.GetWindowThreadProcessId(hwnd)
+                        proc = psutil.Process(pid)
+                        if proc.name() == PROCNAME:
+                            title = get_window_title(hwnd)
+                            if "-" in title:
+                                return True
+                    except (psutil.NoSuchProcess, psutil.AccessDenied):
+                        continue
             return False
 
-    ctypes.windll.user32.BlockInput(True)
+        timeout = time.time() + 1
+        while not is_spotify_ready():
+            if time.time() > timeout:
+                break
+            time.sleep(0.2)
 
-    # Kill existing Spotify
-    for proc in psutil.process_iter():
-        if proc.name() == PROCNAME:
-            proc.kill()
+        pg.press('space')
+        time.sleep(0.3)
+        pg.press('nexttrack')
 
-    time.sleep(0.5)
-    os.system("start spotify")
+        ctypes.windll.user32.ShowWindow(foreground, 6)  # Minimize
+        pg.click()
 
-    # Wait until Spotify window is fully open
-    
-    def is_spotify_ready():
-        hwnds = []
-
-        def collect(hwnd, _):
-            hwnds.append(hwnd)
-        win32gui.EnumWindows(collect, None)
-
-        for hwnd in hwnds:
-            if win32gui.IsWindowVisible(hwnd):
-                i, pid = win32process.GetWindowThreadProcessId(hwnd)
-                
-                try:
-                    
-                    proc = psutil.Process(pid)
-                    if proc.name() == PROCNAME:
-                        title = get_window_title(hwnd)
-                        # Ads don't have a dash
-                        if "-" in title:
-                            return True
-                        
-                except psutil.NoSuchProcess:
-                    continue
-        return False
-
-	#extra 2 seconds
-    timeout = time.time() + 2
-    while not is_spotify_ready():
-        if time.time() > timeout:
-            break
-        time.sleep(0.2)
-
-    pg.press('space')
-    time.sleep(0.3)
-    pg.hotkey('fn', 'f9')
-
-    ctypes.windll.user32.ShowWindow(ctypes.windll.user32.GetForegroundWindow(), 6)
-    pg.click()
-    ctypes.windll.user32.BlockInput(False)
-
+    finally:
+        ctypes.windll.user32.BlockInput(False)
 
 def check_window(hwnd, extra):
     if is_spotify_ad_window(hwnd):
@@ -112,3 +94,6 @@ def main():
     while True:
         win32gui.EnumWindows(check_window, None)
         time.sleep(0.1)
+
+if __name__ == "__main__":
+    main()
